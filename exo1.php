aa22365a9d25f64f98c9d686a287f3605cf97888
Leef <mehdi.diouane31@gmail.com>
2022-03-24 10:24:00 +0100

    exo 1 php

---
+++ exo1.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+        $maVariable = 5;
+        echo $maVariable;
+        echo gettype($maVariable);
+?>
+<br>
+<?php
+        $monPrenom = "Mehdi";
+        echo $monPrenom;
+?>
+<br>
+<?php
+    $monBool = FALSE;
+    echo gettype($monBool);
+?>
+
+</body>
+</html>
